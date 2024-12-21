ffa79e825cf8598ddd8d2143b6ef80b4014c7414
Austin Sonderman <sondy91@users.noreply.github.com>
2024-12-20 20:10:39 -0600

    refactor: #1 Day3 Part 2

--- 2024/day3/main.go
+++ 2024/day3/main.go
@@ -11,15 +11,26 @@ import (
 
 func main() {
 	inputData, err := os.ReadFile("input.txt")
-	println("Input length: ", len(inputData))
+
 	if err != nil {
 		log.Fatal("Couldn't read input file")
 	}
+	segments := strings.Split(string(inputData), "don't()")
+	total := findTotalInString(segments[0])
+	for _, segment := range segments[1:] {
+		// Split on do() and skip that first one since don't() disables them
+		// and process any others that are enabled
+		_, doSegments, _ := strings.Cut(segment, "do()")
+		total += findTotalInString(doSegments)
+	}
+	fmt.Println("Total from multiplication commands: ", total)
+}
 
+func findTotalInString(inputData string) int {
 	r, _ := regexp.Compile(`(?m)mul\([0-9]{1,3},[0-9]{1,3}\)`)
 	total := 0
-	commands := r.FindAllString(string(inputData), -1)
-	println(len(commands))
+	commands := r.FindAllString(inputData, -1)
+
 	for i := 0; i < len(commands); i++ {
 		numbers := strings.Split(commands[i], ",")
 
@@ -29,5 +40,5 @@ func main() {
 		rightInt, _ := strconv.Atoi(right)
 		total += (leftInt * rightInt)
 	}
-	fmt.Println("Total from multiplication commands: ", total)
+	return total
 }
